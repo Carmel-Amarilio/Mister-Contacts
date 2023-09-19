954822d278a7e811d63faf5e2e8c23af391cd3e7
Carmel Amarilio <carmelamarilio@gmail.com>
2023-09-19 16:09:58 +0300

    F/contacts details/add contact details

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -1,22 +1,29 @@
 const { Route, Routes } = ReactRouterDOM;
 const Router = ReactRouterDOM.HashRouter;
+const { Provider } = ReactRedux
 
 import { AppHeader } from "./cmps/AppHeader.jsx";
 import { Home } from "./views/Home.jsx";
 import { Teams } from "./views/Teams.jsx";
 import { Contacts } from "./views/Contacts.jsx";
+// import { ContactsDetails } from "./views/ContactsDetails.jsx";
+
+import { store } from './store/store.js'
 
 export function App() {
   return (
-    <Router>
-      <section className="app">
-        <AppHeader />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/teams" element={<Teams />} />
-          <Route path="/contacts" element={<Contacts />} />
-        </Routes>
-      </section>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <section className="app">
+          <AppHeader />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/teams" element={<Teams />} />
+            <Route path="/contacts" element={<Contacts />} />
+            {/* <Route path="/contacts/:contactsId" element={<ContactsDetails />} /> */}
+          </Routes>
+        </section>
+      </Router>
+    </Provider>
   );
 }
